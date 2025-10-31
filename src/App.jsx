7d445c9ed98d54e55c78f970c19ef4246accc3df
gkaizu <gkaizu90@gmail.com>
2025-10-31 09:28:24 +0900

    FormAddFriendをコンポーネント化して動作確認

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Button from "./components/Button";
 import FriendsList from "./components/FriendsList";
+import FormAddFriend from "./components/FormAddFriend";
 
 const initialFriends = [
   {
@@ -77,50 +78,6 @@ export default function App() {
         />
       )}
     </div>
-  );
-}
-
-function FormAddFriend({ onAddFriend }) {
-  const [name, setName] = useState("");
-  const [image, setImage] = useState("https://i.pravatar.cc/48");
-
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    if (!name || !image) return;
-
-    const id = crypto.randomUUID();
-    const newFriend = {
-      id,
-      name,
-      image: `${image}?=${id}`,
-      balance: 0,
-    };
-
-    onAddFriend(newFriend);
-
-    setName("");
-    setImage("https://i.pravatar.cc/48");
-  }
-
-  return (
-    <form className="form-add-friend" onSubmit={handleSubmit}>
-      <label>👬 名前</label>
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-
-      <label>📷 プロフ画像</label>
-      <input
-        type="text"
-        value={image}
-        onChange={(e) => setImage(e.target.value)}
-      />
-
-      <Button>追加する</Button>
-    </form>
   );
 }
 
