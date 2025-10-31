a3e4788aed7099d78e89d7143681f8ea76db8242
gkaizu <gkaizu90@gmail.com>
2025-10-31 09:14:39 +0900

    Friendをコンポーネント化して動作確認

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Button from './components/Button'
+import Friend from './components/Friend'
 
 const initialFriends = [
   {
@@ -93,32 +94,7 @@ function FriendsList({ friends, onSelected, selectedFriend }) {
   );
 }
 
-function Friend({ friend, onSelected, selectedFriend }) {
-  const isSelected = selectedFriend?.id === friend.id;
 
-  return (
-    <li className={isSelected ? "selected" : ""}>
-      <img src={friend.image} alt={friend.name} />
-      <h3>{friend.name}</h3>
-
-      {friend.balance < 0 && (
-        <p className="red">
-          {friend.name} に支払う金額: {Math.abs(friend.balance)} 円
-        </p>
-      )}
-      {friend.balance > 0 && (
-        <p className="green">
-          {friend.name}があなたに払う金額: {Math.abs(friend.balance)} 円
-        </p>
-      )}
-      {friend.balance === 0 && <p>割り勘が完了しています</p>}
-
-      <Button onClick={() => onSelected(friend)}>
-        {isSelected ? "閉じる" : "選択"}
-      </Button>
-    </li>
-  );
-}
 
 function FormAddFriend({ onAddFriend }) {
   const [name, setName] = useState("");
